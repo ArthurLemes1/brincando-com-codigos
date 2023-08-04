6d4ee03ac39c3d7f7e07df6b8fcdfb0fb4da8a14
Arthur Lemes <137119165+ArthurLemes1@users.noreply.github.com>
2023-08-03 21:37:16 -0300

    Add files via upload

---
+++ aul40.js
@@ -0,0 +1,34 @@
+    let n1 = 10 
+    let n2 = 20
+    let n3 = 30 
+
+    console.log(n1, n2, n3)
+    console.log(Number.isInteger(n1))
+    console.log(Number.isInteger(n2))
+    console.log(Number.isInteger(n3))
+
+    n1 = 20.500000
+    n2 = 50
+    n3 = 54.345123
+
+    console.log(Number.isInteger(n1))
+    console.log(Number.isInteger(n2))
+    console.log(Number.isInteger(n3))
+
+    console.log(n1.toFixed(3))
+    console.log(n1.toString)
+    console.log(n1.toString(2))
+
+    console.log(Number.parseInt(n1))
+    console.log(Number.isInteger(n1))
+
+    let nome = 'Arthur'
+    console.log(Number.isInteger(nome))
+    nome = 20 
+    console.log(Number.parseInt(nome))
+
+    console.log((nome + n1) * Math.PI)
+    let total = n1  *  Math.PI
+    console.log(total)
+
+    console.log(total.toFixed(2))
